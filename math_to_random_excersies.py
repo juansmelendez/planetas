5126dd5e17eb64fdd53dacb80712ad93549dfbed
Juan Sebastián <js.melendez@uniandes.edu.co>
2017-07-17 15:59:24 -0500

    Add files via upload

---
+++ math_to_random_excersies.py
@@ -0,0 +1,21 @@
+# ejercicios de math: 7, 11, 13, 16
+# from: http://www.w3resource.com/python-exercises/numpy/python-numpy-math.php#EDITOR
+#ejercicios de random: 19, 20
+# from: http://www.w3resource.com/python-exercises/numpy/python-numpy-random.php
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#####################################################################                 MATH                  ##########
+###########################################################
+
+# 7: Write a Python program to calculate the absolute value element-wise
+
+arreglo1 = np.array([-10.2,122.2,0.2])
+arreglo2 = 
+
+
+#####################################################################                RANDOM                 ##########
+###########################################################
+
